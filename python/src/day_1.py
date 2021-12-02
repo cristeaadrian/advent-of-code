dce2d7a6af972624bbb52bc0afe0d21b69b84170
Adrian Cristea <adrian.cristea@flexitricity.com>
2021-12-02 13:26:18 +0000

    simplified day 1 solution

--- python/src/day_1.py
+++ python/src/day_1.py
@@ -1,18 +1,14 @@
-import itertools as it
-
-
 def get_input() -> list[int]:
     with open("../../inputs/day_1_input.txt", "r") as f:
         return [int(value) for value in f.read().splitlines()]
 
 
 def first_star(values: list[int]) -> int:
-    return sum(it.starmap(lambda x, y: x < y, zip(values, values[1:])))
+    return sum(x < y for x, y in zip(values, values[1:]))
 
 
 def second_star(values: list[int]) -> int:
-    three_measurement_window = [x + y + z for x, y, z in zip(values, values[1:], values[2:])]
-    return sum(it.starmap(lambda x, y: x < y, zip(three_measurement_window, three_measurement_window[1:])))
+    return sum(x < y for x, y in zip(values, values[3:]))
 
 
 if __name__ == "__main__":
