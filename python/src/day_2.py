c502369be1780f2ea1136edd91ee120ca53422c6
Adrian Cristea <adrian.cristea@flexitricity.com>
2021-12-02 13:07:27 +0000

    day_2 exercise complete, and creates both solutions in one pass

--- python/src/day_2.py
+++ python/src/day_2.py
@@ -1,40 +1,21 @@
-from dataclasses import dataclass
-from enum import Enum
-
-
-class Command(str, Enum):
-    up = "up"
-    down = "down"
-    forward = "forward"
-
-
-@dataclass
-class SubmarineCommands:
-    command: Command
-    value: int
-
-
-def get_input() -> list[SubmarineCommands]:
+def get_results() -> tuple[int, int]:
     with open("../../inputs/day_2_input.txt", "r") as f:
-        inputs = map(str.split, f.read().splitlines())
-        processed_commands = [SubmarineCommands(Command(command[0]), int(command[1])) for command in inputs]
-        return processed_commands
+        position, depth, aim = 0, 0, 0
 
+        for line in f.read().splitlines():
+            match line.split():
+                case ("forward", x):
+                    position += int(x)
+                    depth += aim * int(x)
+                case ("up", x):
+                    aim -= int(x)
+                case ("down", x):
+                    aim += int(x)
 
-def first_star(values: list[SubmarineCommands]) -> int:
-    depth: int = sum(
-        -x.value if x.command == Command.up else x.value
-        for x in filter(lambda x: x.command == Command.up or x.command == Command.down, values)
-    )
-    horizontal_position: int = sum(x.value for x in values if x.command == Command.forward)
-    return depth * horizontal_position
-
-
-def second_star(values: list[SubmarineCommands]) -> int:
-    ...
+    return position * aim, position * depth
 
 
 if __name__ == "__main__":
-    input_values: list[SubmarineCommands] = get_input()
-    print(f"Solution 1: {first_star(input_values)}")
-    # print(f"Solution 2: {second_star(input_values)}")
+    first_star, second_star = get_results()
+    print(f"Solution 1: {first_star}")
+    print(f"Solution 2: {second_star}")
