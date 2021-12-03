a870605e5bcb5495c01b681309d86d7f7f3c9bff
Adrian Cristea <cristea.adrian@icloud.com>
2021-12-03 11:54:59 +0000

    added initial day 3 files

---
+++ 2021/python/src/day_3.py
@@ -0,0 +1,20 @@
+from typing import Any
+
+
+def get_input() -> list[Any]:
+    with open("../../inputs/day_3_input.txt", "r") as f:
+        return f.read().splitlines()
+
+
+def first_star() -> int:
+    ...
+
+
+def second_star() -> int:
+    ...
+
+
+if __name__ == "__main__":
+    input_values: list[Any] = get_input()
+    print(f"Solution 1: {first_star()}")
+    print(f"Solution 2: {second_star()}")
