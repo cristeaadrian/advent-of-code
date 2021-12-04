be9329a72e00248ca825aa10d7bd7c4533a3d76e
Adrian Cristea <cristea.adrian@icloud.com>
2021-12-04 12:12:12 +0000

    added second star for day 3

--- 2021/python/src/day_3.py
+++ 2021/python/src/day_3.py
@@ -4,34 +4,46 @@ from src.utils import timer
 
 
 @timer
-def get_input() -> tuple[set[str], list[str], int]:
+def get_input() -> list[str]:
     with open("../../inputs/day_3_input.txt", "r") as f:
         values = f.read().splitlines()
-        digits = len(values[0])
-        distinct_values = set(values)
-        columns = ["".join(value[digit] for value in values) for digit in range(digits)]
-        return distinct_values, columns, digits
+        return values
 
 
 @timer
-def first_star(columns: list[str], digits: int) -> int:
-    column_counter = (Counter(column) for column in columns)
-    most_common_bits = "".join("1" if column["1"] > column["0"] else "0" for column in column_counter)
+def first_star(values: list[str]) -> int:
+    digits = len(values[0])
+    columns = ("".join(value[digit] for value in values) for digit in range(digits))
+    bits_per_column = (Counter(column) for column in columns)
+    most_common_bits = "".join("1" if column["1"] > column["0"] else "0" for column in bits_per_column)
     gamma = int(most_common_bits, 2)
     epsilon = gamma ^ (pow(2, digits) - 1)
     return gamma * epsilon
 
 
 @timer
-def second_star(values: set[str], columns: list[str], digits: int) -> int:
-    ...
+def second_star(values: list[str]) -> int:
+    def calculate_ratings(distinct_values: set[str], digits: int, most_common_bit: bool = True) -> int:
+        for index in range(digits):
+            column = "".join(code[index] for code in distinct_values)
+            if column.count("0") <= column.count("1"):
+                bit = "1" if most_common_bit else "0"
+            else:
+                bit = "0" if most_common_bit else "1"
+            distinct_values -= {code for code in distinct_values if code[index] == bit}
+            if len(distinct_values) == 1:
+                return int(distinct_values.pop(), 2)
+
+    oxygen = calculate_ratings(set(values), len(values[0]))
+    co2 = calculate_ratings(set(values), len(values[0]), False)
+    return oxygen * co2
 
 
 @timer
 def main():
-    input_distinct_values, input_columns, input_digits = get_input()
-    print(f"Solution 1: {first_star(input_columns, input_digits)}")
-    print(f"Solution 2: {second_star(input_distinct_values, input_columns, input_digits)}")
+    input_values = get_input()
+    print(f"Solution 1: {first_star(input_values)}")
+    print(f"Solution 2: {second_star(input_values)}")
 
 
 if __name__ == "__main__":
