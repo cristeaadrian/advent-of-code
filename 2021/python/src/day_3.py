1b35271df984becff6c069afbe1505adb3fcb976
Adrian Cristea <cristea.adrian@icloud.com>
2021-12-04 09:40:53 +0000

    moved input logic out of the solution functions

--- 2021/python/src/day_3.py
+++ 2021/python/src/day_3.py
@@ -1,14 +1,20 @@
 from collections import Counter
 
+from src.utils import timer
 
-def get_input() -> list[str]:
+
+@timer
+def get_input() -> tuple[set[str], list[str], int]:
     with open("../../inputs/day_3_input.txt", "r") as f:
-        return f.read().splitlines()
+        values = f.read().splitlines()
+        digits = len(values[0])
+        distinct_values = set(values)
+        columns = ["".join(value[digit] for value in values) for digit in range(digits)]
+        return distinct_values, columns, digits
 
 
-def first_star(values: list[str]) -> int:
-    digits = len(values[0])
-    columns = ("".join(value[digit] for value in values) for digit in range(digits))
+@timer
+def first_star(columns: list[str], digits: int) -> int:
     column_counter = (Counter(column) for column in columns)
     most_common_bits = "".join("1" if column["1"] > column["0"] else "0" for column in column_counter)
     gamma = int(most_common_bits, 2)
@@ -16,11 +22,17 @@ def first_star(values: list[str]) -> int:
     return gamma * epsilon
 
 
-def second_star(values: list[str]) -> int:
+@timer
+def second_star(values: set[str], columns: list[str], digits: int) -> int:
     ...
 
 
+@timer
+def main():
+    input_distinct_values, input_columns, input_digits = get_input()
+    print(f"Solution 1: {first_star(input_columns, input_digits)}")
+    print(f"Solution 2: {second_star(input_distinct_values, input_columns, input_digits)}")
+
+
 if __name__ == "__main__":
-    input_values: list[str] = get_input()
-    print(f"Solution 1: {first_star(input_values)}")
-    print(f"Solution 2: {second_star(input_values)}")
+    main()
