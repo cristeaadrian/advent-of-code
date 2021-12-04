d15750950051b00dac26e22b3258554aa8936282
Adrian Cristea <cristea.adrian@icloud.com>
2021-12-04 09:19:56 +0000

    implemented faster, more functional solution for day 3 star 1

--- 2021/python/src/day_3.py
+++ 2021/python/src/day_3.py
@@ -1,19 +1,19 @@
+from collections import Counter
+
+
 def get_input() -> list[str]:
     with open("../../inputs/day_3_input.txt", "r") as f:
         return f.read().splitlines()
 
 
 def first_star(values: list[str]) -> int:
-    digits = [0] * len(values[0])
-    for value in values:
-        for index, digit in enumerate(value):
-            if int(digit) == 1:
-                digits[index] += 1
-            else:
-                digits[index] -= 1
-    gamma_rate = int("".join(["1" if digit >= 0 else "0" for digit in digits]), 2)
-    epsilon_rate = int("".join(["1" if digit < 0 else "0" for digit in digits]), 2)
-    return gamma_rate * epsilon_rate
+    digits = len(values[0])
+    columns = ("".join(value[digit] for value in values) for digit in range(digits))
+    column_counter = (Counter(column) for column in columns)
+    most_common_bits = "".join("1" if column["1"] > column["0"] else "0" for column in column_counter)
+    gamma = int(most_common_bits, 2)
+    epsilon = gamma ^ (pow(2, digits) - 1)
+    return gamma * epsilon
 
 
 def second_star(values: list[str]) -> int:
@@ -22,6 +22,5 @@ def second_star(values: list[str]) -> int:
 
 if __name__ == "__main__":
     input_values: list[str] = get_input()
-    print(f"Received the following input: {input_values}")
     print(f"Solution 1: {first_star(input_values)}")
     print(f"Solution 2: {second_star(input_values)}")
